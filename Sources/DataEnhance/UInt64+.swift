3239b86bc9b437dca55f276e7e57e5d9605f5558
HAL <68320771+HALQME@users.noreply.github.com>
2024-09-20 13:26:01 +0900

    update function

--- Sources/DataEnhance/UInt64+.swift
+++ Sources/DataEnhance/UInt64+.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension UInt64 {
-    var data: Data {
+    public var data: Data {
         var uint64 = self
         let data = Data(bytes: &uint64, count: MemoryLayout<UInt64>.size)
         return data
